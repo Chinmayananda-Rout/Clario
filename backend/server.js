bde7deecaa5a1f8e9c6968a7976e815703ae8000
Chinmayananda Rout <routandrout2003@gmail.com>
2026-05-22 18:17:38 +0530

    added user registration and login

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/auth', require('./routes/authRoutes'));
 
 app.get('/', (req, res) => {
   res.send('Hello from the backend!');
